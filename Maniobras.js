f1e4f05cd44ab582c140a338b788976b0871854b
tcarlosgg <topecarlosgg@gmail.com>
2026-01-16 19:59:01 +0100

    First upload
    Uploaded wip files to test webpage in mobile

---
+++ Maniobras.js
@@ -0,0 +1,80 @@
+maniobras = [
+    /*
+    Nombre:       Nombre de la maniobra
+    Disciplina:   Espíritu Devoto, Dragón de Piedra o Cuervo Blanco     |   Tiene que estar escrito exactamente así o dará error
+    Tipo:         Golpe, Impulso o Contraataque                         |   Tiene que estar escrito exactamente así o dará error
+    Subtipo:      Fuego, Orden, etc. Solo algunas maniobras tienen subtipo. Si no tiene subtipo, deja las comillas vacías: ""
+    Nivel:        Nivel de iniciador mínimo para adquirir la maniobra   |   Recomiendo poner solo el número
+    Acción:       Acción usada para inicial la maniobra: Estándar, rápida, inmediata, etc.
+    Alcance:      Dónde tiene que estar el objetivo: Es un efecto personal, de ataque cuerpo-a-cuerpo, a larga distancia...?
+    Objetivo:     A quién afecta: A ti, a una criatura, un área...?
+    Duración:     La maniobra termina en cuanto se realiza (Instantánea), tiene un efecto persistente...?
+    Condición:    Qué debe cumplirse para que la maniobra surja efecto? Atacar exitosamente, que el enemigo supere una Clase de Dificultad...?
+    Descripción:  Explicación adicional.                                |   Puede ser larga, pero no sé qué pasará si el texto se sale... XD
+    */
+    {
+        "nombre":       "Nombre maniobra 0",
+        "disciplina":   "Disciplina 0",
+        "tipo":         "Tipo 0",
+        "subtipo":      "Subtipo 0",
+        "nivel":        "Nivel 0",
+        "accion":       "Acción 0",
+        "alcance":      "Alcance 0",
+        "objetivo":     "Objetivo 0",
+        "duracion":     "Duración 0",
+        "condicion":    "Condición 0",
+        "descripcion":  "Descripción 0"
+    },
+    {
+        "nombre":       "",
+        "disciplina":   "",
+        "tipo":         "",
+        "subtipo":      "",
+        "nivel":        "",
+        "accion":       "",
+        "alcance":      "",
+        "objetivo":     "",
+        "duracion":     "",
+        "condicion":    "",
+        "descripcion":  ""
+    },
+    {
+        "nombre":       "",
+        "disciplina":   "",
+        "tipo":         "Golpe",
+        "subtipo":      "",
+        "nivel":        "",
+        "accion":       "",
+        "alcance":      "",
+        "objetivo":     "",
+        "duracion":     "",
+        "condicion":    "",
+        "descripcion":  ""
+    },
+    {
+        "nombre":       "",
+        "disciplina":   "",
+        "tipo":         "Impulso",
+        "subtipo":      "",
+        "nivel":        "",
+        "accion":       "",
+        "alcance":      "",
+        "objetivo":     "",
+        "duracion":     "",
+        "condicion":    "",
+        "descripcion":  ""
+    },
+    {
+        "nombre":       "",
+        "disciplina":   "",
+        "tipo":         "Contraataque",
+        "subtipo":      "Orden",
+        "nivel":        "",
+        "accion":       "",
+        "alcance":      "",
+        "objetivo":     "",
+        "duracion":     "",
+        "condicion":    "",
+        "descripcion":  ""
+    }
+];
